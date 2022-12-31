c1e71985cb19272f3465df38d77a592e9d7e55d0
filipe11402 <filipe11402@outlook.pt>
2022-12-31 07:37:33 +0000

    rm of user mixing

--- src/users/users.py
+++ src/users/users.py
@@ -1,6 +1,6 @@
 from src.extensions import db
 
-class User(db.Model, UserMixin):
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(255), unique=True)
 	password = db.Column(db.Unicode(80))
