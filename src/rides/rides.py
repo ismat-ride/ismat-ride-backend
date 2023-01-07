c088d4a8c0f8b966937c1ea44cce5cc51aec2439
filipe11402 <filipe11402@outlook.pt>
2023-01-07 16:32:58 +0000

    implemented base template in user list

--- src/rides/rides.py
+++ src/rides/rides.py
@@ -8,20 +8,6 @@ class RideStatus(db.Model):
     def __str__(self):
         return self.name
 
-class Local(db.Model):
-    __tablename__ = 'local'
-    id =  db.Column(db.Integer, primary_key=True)
-    user_id = db.Column('user_id', db.ForeignKey('user.id')) 
-    user = db.relationship('User', backref='local')
-    name = db.Column(db.String(100))
-    description = db.Column(db.String(150))
-    postal_code = db.Column(db.String(20))
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    createdAt = db.Column(db.DateTime, default=datetime.datetime.now)
-    updatedAt = db.Column(db.DateTime, default=datetime.datetime.now)
-    def __str__(self):
-        return self.name
         
 ride_passengers = db.Table('ride_passengers',
                     db.Column('passenger_id', db.Integer, db.ForeignKey('user.id')),
@@ -47,12 +33,6 @@ class Ride(db.Model):
     updatedAt = db.Column(db.DateTime, default=datetime.datetime.now)
     status_id = db.Column('status_id', db.ForeignKey('ride_status.id'))
     status = db.relationship('RideStatus', backref='ride')
-
-class RideStatus(db.Model):
-    __tablename__ = 'ride_status'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-
 
 class Local(db.Model):
 	__tablename__ = 'local'
