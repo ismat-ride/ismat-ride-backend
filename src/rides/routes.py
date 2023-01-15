34956a1b0ee981cb936f10d32c92712bbeed27da
filipe11402 <filipe11402@outlook.pt>
2023-01-15 18:43:37 +0000

    added page with rides list

--- src/rides/routes.py
+++ src/rides/routes.py
@@ -1,31 +1,49 @@
-from functools import wraps
+from operator import or_
+from sqlalchemy import func
 from flask_login import login_required
 from src.rides.dto.ride_list_dto import RideListDto
 from src.rides.rides import Ride
-from flask import render_template, redirect, url_for, request
-from flask_login import current_user
+from src.users.users import User
+from flask import render_template, request
 from src.rides import rides_bp
-
-def student_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if current_user.type != 'Student':
-            return redirect(url_for('auth.login', next=request.url))
-        return f(*args, **kwargs)
-    return decorated_function
+from src.extensions import student_required, ITEMS_PER_PAGE
 
 @rides_bp.route("list")
 @login_required
 @student_required
 def get():
-      db_rides = Ride.query.all()
+    page = request.args.get('page', 1, type=int)
 
-      rides_list = list()
+    query = Ride.query
 
-      for ride in db_rides:
-            rides_list.append(
-                  RideListDto(ride.driver.get_full_name(),ride.origin, ride.status.name, 
-                  ride.start_time.strftime('%d-%m-%Y'), ride.start_time.strftime('%H:%M'), ride.seats, ride.seats - len(ride.passengers)) 
-      )
+    if request.args.get("name"):
+        query = query.filter(or_(
+            User.first_name.contains(request.args.get("name")),
+            User.last_name.contains(request.args.get("name"))
+            ))
+    if request.args.get("origin"):
+        query = query.filter(or_(Ride.origin.contains(request.args.get("origin")),
+        Ride.destiny.contains(request.args.get("origin"))))
+    if request.args.get("date"):
+        query = query.filter(func.date(Ride.createdAt) == request.args.get("date"))
+    if request.args.get("status"):
+        query = query.filter(Ride.status_id == request.args.get("status"))
 
-      return render_template("rides/index.html", rides = rides_list)
+    query = query.paginate(page=page, per_page=ITEMS_PER_PAGE)
+
+    response = {'items': list(), 'iter_pages': query.iter_pages, 'page': page, 'pages': query.pages, 'next_num': query.next_num}
+
+    rides_list = list()
+    
+    for ride in query:
+        rides_list.append(
+            RideListDto(ride.driver.get_full_name(),ride.origin, ride.status.name, 
+            ride.start_time.strftime('%d-%m-%Y'), ride.start_time.strftime('%H:%M'), ride.seats, ride.seats - len(ride.passengers)) 
+        )
+
+    response['items'] = rides_list
+
+    if rides_list.__len__() == 0:
+        return(render_template("rides/no_data.html"))
+
+    return render_template("rides/index.html", request_list = response)
