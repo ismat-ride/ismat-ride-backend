bb6b6c7b2b62e4e25dc24c66a6b530eef41d252b
DimiHK <dimihtsk@gmail.com>
2023-01-16 18:42:24 +0000

    added edit vehicle

--- src/vehicles/dto/vehicles_dto.py
+++ src/vehicles/dto/vehicles_dto.py
@@ -2,7 +2,9 @@ from dataclasses import dataclass
 
 @dataclass
 class VehicleDto:
+    id:str
     brand: str
     model: str
     color: str
     license_plate: str
+    seats: str
--- src/vehicles/routes.py
+++ src/vehicles/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
-from src.extensions import ITEMS_PER_PAGE
+
+from flask import flash, redirect, render_template, request, url_for
+from src.extensions import db,ITEMS_PER_PAGE
 from src.rides.rides import Ride
 from src.users.users import Brand, Model, Vehicle
 from src.vehicles import vehicles_bp
@@ -10,6 +11,8 @@ def vehicles_list():
     page = request.args.get('page', 1, type=int)
 
     query = Vehicle.query
+
+    models_list = Model.query.all()
 
     if request.args.get("model"):
         query = query.join(Model, Vehicle.model).filter(Model.name.contains(request.args.get("model")))
@@ -26,7 +29,7 @@ def vehicles_list():
     
     for vehicle in query:
         vehicle_list.append(
-            VehicleDto(vehicle.model.brand.name, vehicle.model.name, vehicle.color, vehicle.license_plate) 
+            VehicleDto(vehicle.id, vehicle.model.brand.name, vehicle.model.name, vehicle.color, vehicle.license_plate, vehicle.seats) 
         )
 
     response['items'] = vehicle_list
@@ -34,5 +37,44 @@ def vehicles_list():
     if vehicle_list.__len__() == 0:
         return(render_template("vehicle/no_data.html"))
 
-    return render_template("vehicle/index.html", vehicles = response)    
+    return render_template("vehicle/index.html", vehicles = response, models = models_list)    
 
+@vehicles_bp.route("/delete/<id>", methods = ["POST"])
+def delete_vehicle(id):
+    
+    vehicle_to_delete = db.session.query(Vehicle).filter(Vehicle.id == id).one()
+
+    if vehicle_to_delete is None:
+        flash('Este veículo não existe', "error")
+        return redirect(url_for('vehicles.vehicles_list'))
+
+    exists = bool(db.session.query(Ride).filter_by(vehicle_id = id).first())
+   
+    if exists is True:
+        flash("Veículo está a ser utilizado, não pode ser apagado!" , "error")
+        return redirect(url_for('vehicles.vehicles_list'))
+   
+    db.session.delete(vehicle_to_delete)
+    db.session.commit()
+
+    flash("Veículo deletado com sucesso!" , "info")
+    return redirect(url_for('vehicles.vehicles_list'))
+
+@vehicles_bp.route("/edit/<id>", methods = ["POST"])
+def edit_vehicle(id):
+    vehicle = Vehicle.query.filter_by(id=id).first()
+
+    if vehicle is None:
+        flash('Não existe este veículo', 'error')
+        return redirect(url_for('vehicles.vehicles_list'))
+        
+    vehicle.model_id = request.form.get('model')
+    vehicle.color = request.form.get('color')
+    vehicle.license_plate = request.form.get('vin')
+    vehicle.seats = request.form.get('places')
+
+    db.session.commit()
+
+    flash('Veículo atualizado com sucesso', 'info')
+
+    return redirect(url_for('vehicles.vehicles_list'))  
