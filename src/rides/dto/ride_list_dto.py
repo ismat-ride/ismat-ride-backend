190887c82135bec3edcf2443cf052b2d31c91528
DimiHK <dimihtsk@gmail.com>
2023-01-20 17:25:56 +0000

    fixed issues

--- src/rides/dto/ride_list_dto.py
+++ src/rides/dto/ride_list_dto.py
@@ -4,10 +4,30 @@ from dataclasses import dataclass
 class RideListDto:
     id: int
     driver_name: str
+    driver_initials: str
     starting_location: str
+    end_location: str
     status: str
     starting_date: str
     starting_hour: str
     total_places: str
     total_available_places: str
+    is_joinable: bool
+
+@dataclass
+class RideDto:
+    id: int
+    starting_location: str
+    end_location: str
+    vehicle: str
+    starting_date: str
+    starting_hour: str
+    total_places: str
+    passengers: list
+    is_joinable: bool
+    
+@dataclass
+class PassengerListDto:
+    id: int
+    initials: str
  
